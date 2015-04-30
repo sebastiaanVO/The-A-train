d42b2022d538eb727088e956509519e47d0a299f
JeroenRobben <JeroenRobben@users.noreply.github.com>
2015-04-30 02:49:30 +0200

    Update callibreren_draaien.ino

--- callibreren_draaien.ino
+++ callibreren_draaien.ino
@@ -2,42 +2,53 @@
 const unsigned int DCP[] = {13, 2, 3, 4, 5, 6, 7, 8, 9};
 const unsigned int ACP[] = {13, A0, A1, A2, A3, A4, A5};
 
-int motor_l_p = DCP[5];
-int motor_r_p = ACP[4];
+int motor_l_p = DCP[1];
+int motor_r_p = DCP[2];
+int relais_l_p = DCP[3];
+int relais_r_p = DCP[4];
+
+//richting, 0 links, 1 rechts
+int richting = 0;
+
+int draaitijd_ms = 200;
+
+//snelheid motoren, tussen 0 en 255
+int snelheid_motor_l = 100;
+int snelheid_motor_r = 100;
+
 
 void setup(){
   Serial.begin(9600);
+  pinMode(motor_l_p, OUTPUT);
+  pinMode(motor_r_p, OUTPUT);
+  pinMode(relais_l_p, OUTPUT);
+  pinMode(relais_r_p, OUTPUT);
+  
 }
 
-void loop(){
-    long totaal_links = 0;
-    int gemiddelde_links = 0;
-  
-    long totaal_rechts = 0;
-    int gemiddelde_rechts = 0;
-    
-    long totaal_voor = 0;
-    int gemiddelde_voor = 0;
-  
-  for(int count = 0;count < 100;count++){
-    totaal_links += analogRead(afstand_l_p);
-    totaal_rechts += analogRead(afstand_r_p);
-    totaal_voor = analogRead(afstand_v_p);
-    delay(5);
-  }
-  
-    gemiddelde_links = totaal_links / 100;
-    gemiddelde_rechts = totaal_rechts / 100;
-  
-    Serial.print("Links: ");
-    Serial.print(gemiddelde_links);
-    Serial.println();
-    
-    Serial.print("Rechts: ");
-    Serial.print(gemiddelde_rechts);
-    Serial.println();
 
-    Serial.print("Voor: ");
-    Serial.print(gemiddelde_voor);
-    Serial.println();
+
+void loop(){
+//Relais instellen
+//Relais links
+if (richting == 1){
+	digitalWrite(relais_l_p, HIGH);
+	digitalWrite(relais_r_p, LOW);
+}
+
+else if (richting == 0){
+	digitalWrite(relais_l_p, LOW);
+	digitalWrite(relais_r_p, HIGH);
+}
+	
+//Schrijven motoren
+ analogWrite(motor_l_p, snelheid_motor_l);
+ analogWrite(motor_r_p, snelheid_motor_r);
+ 
+ delay(draaitijd_ms);
+ 
+ analogWrite(motor_l_p, 0);
+ analogWrite(motor_r_p, 0);
+ 
+ delay(10000);
 }
